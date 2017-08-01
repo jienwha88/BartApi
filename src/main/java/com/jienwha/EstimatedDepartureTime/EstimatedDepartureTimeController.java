3344ebe4cd569c04bfed8684c44e9ad50a1b4ea5
William Wu <william_wu@homedepot.com>
2017-07-31 22:23:54 -0400

    adding estimated time of departure endpoint and swagger

--- src/main/java/com/jienwha/EstimatedDepartureTime/EstimatedDepartureTimeController.java
+++ src/main/java/com/jienwha/EstimatedDepartureTime/EstimatedDepartureTimeController.java
@@ -1,13 +1,44 @@
 package com.jienwha.EstimatedDepartureTime;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jienwha.EstimatedDepartureTime.model.EstimatedDepartureTime;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
 
 /**
  * Created by williamwu on 7/31/17.
  */
+@Api
 @RestController
 public class EstimatedDepartureTimeController {
 
 
+
+    @ApiOperation(value = "Get the Estimated Time of Departure for the given Station", response = String.class)
+    @GetMapping (value = "api/v1/etd/{station}")
+    public ResponseEntity<String> getEstimatedDeparturesForStation(@PathVariable("station") String station) throws JsonProcessingException {
+
+        String url = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=MONT&key=MW9S-E7SL-26DU-VV8V&json=y";
+
+        EstimatedDepartureTime edt = new RestTemplate().getForObject(url, EstimatedDepartureTime.class);
+
+        return new ResponseEntity<String>(new ObjectMapper().writeValueAsString(edt), HttpStatus.OK);
+    }
+
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView swaggerui() {
+        return new ModelAndView("redirect:swagger-ui.html");
+    }
 
 }
--- src/main/java/com/jienwha/config/SwaggerConfig.java
+++ src/main/java/com/jienwha/config/SwaggerConfig.java
@@ -1,7 +1,36 @@
 package com.jienwha.config;
 
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 /**
  * Created by williamwu on 7/31/17.
  */
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .build()
+                .apiInfo(metadata());
+    }
+
+    private ApiInfo metadata() {
+        return new ApiInfoBuilder()
+                .title("Bart API")
+                .description("REST services for Bart API")
+                .version("1.0")
+                .contact("William Wu")
+                .build();
+    }
+
 }
